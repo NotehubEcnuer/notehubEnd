d3b1428496d2f6f1b2b7a9057af3f4ddb0df20f5
onion <969023014@qq.com>
2019-11-01 20:21:15 +0800

    initialized project

---
+++ src/main/java/com/ecnu/notehub/NoteApplication.java
@@ -0,0 +1,15 @@
+package com.ecnu.notehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author onion
+ * @date 2019/11/1 -8:17 下午
+ */
+@SpringBootApplication
+public class NoteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NoteApplication.class, args);
+    }
+}
